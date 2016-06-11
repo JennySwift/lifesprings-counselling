d1b70b544760d2154de24e1812ec6581c5fd1810
Jenny Swift <cheezyspaghetti@gmail.com>
2016-06-11 16:01:12 +1000

    Layout for appointment/contact page

--- resources/views/appointment-and-contact.blade.php
+++ resources/views/appointment-and-contact.blade.php
@@ -5,43 +5,54 @@
     <title>Lifesprings Counselling</title>
     @include('main.shared.head-links')
 </head>
-<body id="appointment-and-contact-page" class="scrollbar-container">
+<body id="appointment-and-contact-page" class="">
 
-<navbar></navbar>
+<div class="main">
+    <div class="navbar-text-container-container">
+        <div class="navbar-text-container">
+            <navbar></navbar>
+            <div class="text-container scrollbar-container">
+                <div class="text">
+                    {{--<h1>Appointment and Contact</h1>--}}
 
+                    <h1 class="">
+                        <div>To make an appointment call Dina on</div>
+                        <div><a href="tel:0414440727">&#48;&#52;&#49;&#52; &#52;&#52;&#48; &#55;&#50;&#55;</a>.</div>
+                    </h1>
 
+                    {{--<h1 class="smaller-screens">--}}
+                        {{--<span>To make an appointment call Dina on</span>--}}
+                        {{--<span><a href="tel:0414440727">&#48;&#52;&#49;&#52; &#52;&#52;&#48; &#55;&#50;&#55;</a>.</span>--}}
+                    {{--</h1>--}}
 
-{{--<div class="cover"></div>--}}
-<div class="text">
-    {{--<h1>Appointment and Contact</h1>--}}
-    <h1 class="smaller-screens">
-        <span>To make an appointment call Dina on</span>
-        <span><a href="tel:0414440727">&#48;&#52;&#49;&#52; &#52;&#52;&#48; &#55;&#50;&#55;</a>.</span>
-    </h1>
+                    {{--<h1 class="bigger-screens">To make an appointment call Dina on <a href="tel:0414440727">&#48;&#52;&#49;&#52; &#52;&#52;&#48; &#55;&#50;&#55;</a>.</h1>--}}
 
-    <h1 class="bigger-screens">To make an appointment call Dina on <a href="tel:0414440727">&#48;&#52;&#49;&#52; &#52;&#52;&#48; &#55;&#50;&#55;</a>.</h1>
+                    <p>Appointments are available at Waterloo or Ramsgate as follows:</p>
 
-    <p>Appointments are available at Waterloo or Ramsgate as follows:</p>
+                    <div class="time">
+                        <p><span class="bold">Tuesdays and Thursdays</span> (12pm-7:30pm last appointment)</p>
+                        <p>Location:  20/198-222 Young Street, Waterloo (inner Sydney) <a target="_blank" href="https://www.google.com.au/maps/place/20%2F198-222+Young+St,+Waterloo+NSW+2017">Map</a></p>
+                    </div>
 
-    <div class="time">
-        <p><span class="bold">Tuesdays and Thursdays</span> (12pm-7:30pm last appointment)</p>
-        <p>Location:  20/198-222 Young Street, Waterloo (inner Sydney) <a target="_blank" href="https://www.google.com.au/maps/place/20%2F198-222+Young+St,+Waterloo+NSW+2017">Map</a></p>
+                    <div class="time">
+                        <p><span class="bold">Wednesday</span> (5- 8 pm last appointment)</p>
+                        <p>Location:  245 Rocky Point Road, Ramsgate (south Sydney)</p>
+                        <p>
+                            <a target="_blank" href="https://www.google.com.au/maps/place/245+Rocky+Point+Rd,+Ramsgate+NSW+2217">Map</a></p>
+                    </div>
+                </div>
+
+                <div class="img-container">
+                    <img src="/img/waterfall.jpg">
+                </div>
+
+            </div>
+        </div>
     </div>
 
-    <div class="time">
-        <p><span class="bold">Wednesday</span> (5- 8 pm last appointment)</p>
-        <p>Location:  245 Rocky Point Road, Ramsgate (south Sydney)</p>
-        <p>
-            <a target="_blank" href="https://www.google.com.au/maps/place/245+Rocky+Point+Rd,+Ramsgate+NSW+2217">Map</a></p>
-    </div>
 
 </div>
 
-
-<div class="img-container">
-    <img src="/img/waterfall.jpg">
-</div>
 @include('main.shared.footer')
-
 </body>
 </html>
--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -1,76 +1,156 @@
-<!doctype html>
+{{--<!doctype html>--}}
+{{--<html lang="en">--}}
+{{--<head>--}}
+    {{--<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">--}}
+    {{--<title>Lifesprings Counselling</title>--}}
+    {{--@include('main.shared.head-links')--}}
+{{--</head>--}}
+{{--<body id="service-page" class="scrollbar-container">--}}
+
+{{--<div class="main">--}}
+    {{--<div class="navbar-text-container-container">--}}
+        {{--<div class="navbar-text-container">--}}
+            {{--<navbar></navbar>--}}
+            {{--<div class="text-container scrollbar-container">--}}
+                {{--<div class="text">--}}
+                    {{--<h1>Service</h1>--}}
+
+                    {{--<img src="/img/forest.jpg">--}}
+
+                    {{--<p>A GP referral is not required to make an appointment.</p>--}}
+
+                    {{--<p>Counselling and therapy are available for the following:</p>--}}
+
+                    {{--<div class="list">--}}
+                        {{--<ul>--}}
+                            {{--<li>Stress management</li>--}}
+                            {{--<li>Anxiety</li>--}}
+                            {{--<li>Anger Management</li>--}}
+                            {{--<li>Grief and Loss</li>--}}
+                            {{--<li>Trauma recovery</li>--}}
+                            {{--<li>Identity issues</li>--}}
+                            {{--<li>Self esteem</li>--}}
+                            {{--<li>Boundaries</li>--}}
+                            {{--<li>Life transitions</li>--}}
+                            {{--<li>Emotional crisis</li>--}}
+                        {{--</ul>--}}
+
+                        {{--<ul>--}}
+                            {{--<li>Addictions</li>--}}
+                            {{--<li>Cultural conflicts</li>--}}
+                            {{--<li>Unhealthy patterns of behaviour</li>--}}
+                            {{--<li>Relationship counselling</li>--}}
+                            {{--<li>Marriage counselling</li>--}}
+                            {{--<li>Pre-marriage counselling</li>--}}
+                            {{--<li>Separation and divorce recovery</li>--}}
+                            {{--<li>Communication</li>--}}
+                            {{--<li>Conflict resolution</li>--}}
+                        {{--</ul>--}}
+                    {{--</div>--}}
+
+                    {{--<p>Fees are charged at the following rates:</p>--}}
+
+                    {{--<ul>--}}
+                        {{--<li>Individual sessions  $80 (50 minutes)</li>--}}
+                        {{--<li>Couple sessions  $120 (80 minutes)</li>--}}
+                    {{--</ul>--}}
+
+                    {{--<p>Concessions are available for full time students and those genuinely in need.</p>--}}
+
+                    {{--<p>Please note that Medicare rebates are not available for counselling.</p>--}}
+
+                    {{--<p>The cost of this service will typically be around the same or lower than the out of pocket cost (gap) of seeing a psychologist under Medicare.</p>--}}
+                {{--</div>--}}
+
+            {{--</div>--}}
+
+        {{--</div>--}}
+    {{--</div>--}}
+
+
+{{--</div>--}}
+
+{{--@include('main.shared.footer')--}}
+
+{{--</body>--}}
+{{--</html>--}}
+
+        <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <title>Lifesprings Counselling</title>
     @include('main.shared.head-links')
 </head>
-<body id="service-page" class="scrollbar-container">
+<body id="service-page" class="">
 
-<navbar></navbar>
+<div class="main">
+    <div class="navbar-text-container-container">
+        <div class="navbar-text-container">
+            <navbar></navbar>
+            <div class="text-container scrollbar-container">
+                <div class="text">
+                    <img src="/img/forest.jpg">
+                    <h1>Service</h1>
 
-<div class="text-img-container">
-    {{--<div class="cover"></div>--}}
-    <div class="text">
+                    <p>A GP referral is not required to make an appointment.</p>
 
-        <h1>Service</h1>
+                    <p>Counselling and therapy are available for the following:</p>
 
-        <p>A GP referral is not required to make an appointment.</p>
+                    <div class="services-fees-container">
+                        <div class="list">
+                            <ul>
+                                <li>Stress management</li>
+                                <li>Anxiety</li>
+                                <li>Anger Management</li>
+                                <li>Grief and Loss</li>
+                                <li>Trauma recovery</li>
+                                <li>Identity issues</li>
+                                <li>Self esteem</li>
+                                <li>Boundaries</li>
+                                <li>Life transitions</li>
+                                <li>Emotional crisis</li>
+                            </ul>
 
-        <p>Counselling and therapy are available for the following:</p>
+                            <ul>
+                                <li>Addictions</li>
+                                <li>Cultural conflicts</li>
+                                <li>Unhealthy patterns of behaviour</li>
+                                <li>Relationship counselling</li>
+                                <li>Marriage counselling</li>
+                                <li>Pre-marriage counselling</li>
+                                <li>Separation and divorce recovery</li>
+                                <li>Communication</li>
+                                <li>Conflict resolution</li>
+                            </ul>
+                        </div>
 
-        <div class="list">
-            <ul>
-                <li>Stress management</li>
-                <li>Anxiety</li>
-                <li>Anger Management</li>
-                <li>Grief and Loss</li>
-                <li>Trauma recovery</li>
-                <li>Identity issues</li>
-                <li>Self esteem</li>
-                <li>Boundaries</li>
-                <li>Life transitions</li>
-                <li>Emotional crisis</li>
-            </ul>
+                        <div class="fees">
+                            <p>Fees are charged at the following rates:</p>
 
-            <ul>
-                <li>Addictions</li>
-                <li>Cultural conflicts</li>
-                <li>Unhealthy patterns of behaviour</li>
-                <li>Relationship counselling</li>
-                <li>Marriage counselling</li>
-                <li>Pre-marriage counselling</li>
-                <li>Separation and divorce recovery</li>
-                <li>Communication</li>
-                <li>Conflict resolution</li>
-            </ul>
+                            <ul>
+                                <li>Individual sessions  $80 (50 minutes)</li>
+                                <li>Couple sessions  $120 (80 minutes)</li>
+                            </ul>
+
+                            <p>Concessions are available for full time students and those genuinely in need.</p>
+
+                            <p>Please note that Medicare rebates are not available for counselling.</p>
+
+                            <p>The cost of this service will typically be around the same or lower than the out of pocket cost (gap) of seeing a psychologist under Medicare.</p>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+
         </div>
-
-        <p>Fees are charged at the following rates:</p>
-
-        <ul>
-            <li>Individual sessions  $80 (50 minutes)</li>
-            <li>Couple sessions  $120 (80 minutes)</li>
-        </ul>
-
-        <p>Concessions are available for full time students and those genuinely in need.</p>
-
-        <p>Please note that Medicare rebates are not available for counselling.</p>
-
-        <p>The cost of this service will typically be around the same or lower than the out of pocket cost (gap) of seeing a psychologist under Medicare.</p>
-
     </div>
 
-    <div class="img-container">
-        {{--<img src="/img/forest.jpg">--}}
-        <img class="smaller-screens" src="/img/river.jpg">
-        <img class="bigger-screens" src="/img/natural.jpg">
-    </div>
+
 </div>
 
-
-
 @include('main.shared.footer')
-
 </body>
 </html>
