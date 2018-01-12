85f44dafe85772bed4bf6809ebab6819a3f69c9e
Jenny Swift <cheezyspaghetti@gmail.com>
2018-01-12 22:20:15 +1100

    Update appointment page

--- resources/views/appointment-and-contact.blade.php
+++ resources/views/appointment-and-contact.blade.php
@@ -26,18 +26,17 @@
 
                     {{--<h1 class="bigger-screens">To make an appointment call Dina on <a href="tel:0414440727">&#48;&#52;&#49;&#52; &#52;&#52;&#48; &#55;&#50;&#55;</a>.</h1>--}}
 
-                    <p>Appointments are available at Waterloo or Ramsgate as follows:</p>
+                    <p>Appointments are available at Rockdale and Waterloo as follows:</p>
 
                     <div class="time">
-                        <p><span class="bold">Tuesdays and Thursdays</span> (12pm-7:30pm last appointment)</p>
-                        <p>Location:  20/198-222 Young Street, Waterloo (inner Sydney) <a target="_blank" href="https://www.google.com.au/maps/place/20%2F198-222+Young+St,+Waterloo+NSW+2017">Map</a></p>
+                        <p><span class="bold">Tuesdays (morning and afternoon)</span></p>
+                        <p>Location: 4/39-45 George Street, Rockdale <a target="_blank" href="https://www.google.com.au/maps/place/4%2F39-45+George+St,+Rockdale+NSW+2216/@-33.9518246,151.1382017,17z/data=!3m1!4b1!4m5!3m4!1s0x6b12b0aa9debfd17:0xebce859f78e8dccf!8m2!3d-33.9518246!4d151.1403904?dcr=0">Map</a></p>
+
                     </div>
 
                     <div class="time">
-                        <p><span class="bold">Wednesday</span> (1-8pm last appointment)</p>
-                        <p>Location:  245 Rocky Point Road, Ramsgate (south Sydney)</p>
-                        <p>
-                            <a target="_blank" href="https://www.google.com.au/maps/place/245+Rocky+Point+Rd,+Ramsgate+NSW+2217">Map</a></p>
+                        <p><span class="bold">Wednesdays and Thursdays (afternoon and evening)</span></p>
+                        <p>Location: 20/198-222 Young Street, Waterloo <a target="_blank" href="https://www.google.com.au/maps/place/198%2F222+Young+St,+Waterloo+NSW+2017/@-33.8997928,151.2062749,17z/data=!3m1!4b1!4m5!3m4!1s0x6b12b1c2c8d009c1:0x48f72a920ef52fd3!8m2!3d-33.8997928!4d151.2084636?dcr=0">Map</a></p>
                     </div>
                 </div>
 
