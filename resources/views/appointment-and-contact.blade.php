0b7e3ab15ed3601a9e79a4b8aa63225713968312
Jenny Swift <cheezyspaghetti@gmail.com>
2020-01-16 10:49:17 +1100

    Remove location from appointment page

--- resources/views/appointment-and-contact.blade.php
+++ resources/views/appointment-and-contact.blade.php
@@ -26,13 +26,7 @@
 
                     {{--<h1 class="bigger-screens">To make an appointment call Dina on <a href="tel:0414440727">&#48;&#52;&#49;&#52; &#52;&#52;&#48; &#55;&#50;&#55;</a>.</h1>--}}
 
-                    <p>Appointments are available at Rockdale and Waterloo as follows:</p>
-
-                    <div class="time">
-                        <p><span class="bold">Tuesdays (morning and afternoon)</span></p>
-                        <p>Location: 4/39-45 George Street, Rockdale <a target="_blank" href="https://www.google.com.au/maps/place/4%2F39-45+George+St,+Rockdale+NSW+2216/@-33.9518246,151.1382017,17z/data=!3m1!4b1!4m5!3m4!1s0x6b12b0aa9debfd17:0xebce859f78e8dccf!8m2!3d-33.9518246!4d151.1403904?dcr=0">Map</a></p>
-
-                    </div>
+                    <p>Appointments are available at Waterloo as follows:</p>
 
                     <div class="time">
                         <p><span class="bold">Wednesdays and Thursdays (afternoon and evening)</span></p>
