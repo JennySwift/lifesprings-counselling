193afb6f3f44e900f196c1d24fa3f8f918c7d9f8
Jenny Swift <cheezyspaghetti@gmail.com>
2023-01-13 19:07:13 +1100

    Update appointment page

--- resources/views/appointment-and-contact.blade.php
+++ resources/views/appointment-and-contact.blade.php
@@ -15,8 +15,8 @@
                 <div class="text">
 
                     <h1 class="">
-                        <div>To make an appointment call Dina on</div>
-                        <div><a href="tel:0414440727">&#48;&#52;&#49;&#52; &#52;&#52;&#48; &#55;&#50;&#55;</a>.</div>
+                        <div>To make an enquiry or book an appointment please contact Dina on</div>
+                        <div><a href="tel:0414440727">&#48;&#52;&#49;&#52; &#52;&#52;&#48; &#55;&#50;&#55;</a></div>
                     </h1>
 
                     {{--<h1 class="smaller-screens">--}}
@@ -26,11 +26,13 @@
 
                     {{--<h1 class="bigger-screens">To make an appointment call Dina on <a href="tel:0414440727">&#48;&#52;&#49;&#52; &#52;&#52;&#48; &#55;&#50;&#55;</a>.</h1>--}}
 
-                    <p>Appointments are available at Waterloo as follows:</p>
+                    <p>Appointments are available on Tuesdays and Wednesdays.</p>
 
                     <div class="time">
-                        <p><span class="bold">Wednesdays and Thursdays (afternoon and evening)</span></p>
-                        <p>Location: 20/198-222 Young Street, Waterloo <a target="_blank" href="https://www.google.com.au/maps/place/198%2F222+Young+St,+Waterloo+NSW+2017/@-33.8997928,151.2062749,17z/data=!3m1!4b1!4m5!3m4!1s0x6b12b1c2c8d009c1:0x48f72a920ef52fd3!8m2!3d-33.8997928!4d151.2084636?dcr=0">Map</a></p>
+                        <p>Location: Life Medical Clinic Botany</p>
+                        <p>Shop G01/1084 Botany Road, Botany</p>
+
+                             {{--<a target="_blank" href="https://www.google.com.au/maps/place/198%2F222+Young+St,+Waterloo+NSW+2017/@-33.8997928,151.2062749,17z/data=!3m1!4b1!4m5!3m4!1s0x6b12b1c2c8d009c1:0x48f72a920ef52fd3!8m2!3d-33.8997928!4d151.2084636?dcr=0">Map</a></p>--}}
                     </div>
                 </div>
 
